32eb010153c5d129252cf448ac1d71c6b2c30657
Zheludevaat <161398877+Zheludevaat@users.noreply.github.com>
2025-07-14 23:49:37 +0200

    Fix Windows launch script

--- launch.js
+++ launch.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 function run(command, args) {
   return new Promise((resolve, reject) => {
-    const cmd = spawn(command, args, { stdio: 'inherit' });
+    const cmd = spawn(command, args, { stdio: 'inherit', shell: true });
     cmd.on('close', code => {
       if (code === 0) resolve();
       else reject(new Error(`${command} exited with code ${code}`));
