2b6e7acd9ddda4e11b6c1e19aec9690b80e016d7
Zheludevaat <161398877+Zheludevaat@users.noreply.github.com>
2025-07-15 21:14:21 +0200

    Normalize IDs and fix tab events

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+
+// Use a writable directory for cached data to avoid Windows permission errors
+const userDir = path.join(app.getPath('temp'), 'tarot-explorer');
+app.setPath('userData', userDir);
+app.commandLine.appendSwitch('disk-cache-dir', path.join(userDir, 'Cache'));
 function createWindow () {
-  const win = new BrowserWindow({ width: 1000, height: 800, webPreferences: { nodeIntegration: false } });
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
   win.loadFile('index.html');
 }
 app.whenReady().then(createWindow);
